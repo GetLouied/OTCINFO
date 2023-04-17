6cb75c92d0674fc26ffd72ec1612d55db780e860
GetLouied <derienzo_louis@yahoo.com>
2023-04-17 19:10:02 -0400

    Initial Push

---
+++ otcfeedparser.py
@@ -0,0 +1,17 @@
+import feedparser
+import requests
+import pandas as pd
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'accept-language': 'en-US,en-CA;q=0.9,en;q=0.8,hi-IN;q=0.7,hi;q=0.6'}
+response = requests.get(
+    'https://www.otcmarkets.com/syndicate/rss.xml', headers=headers)
+
+otc_feed = feedparser.parse(response.content.decode('utf-8'))
+
+for entry in otc_feed.entries:
+    title = entry.title
+    link = entry.link
+    symbol = entry.pink_symbol
+    type = entry.pink_type
+    print(title, link)
